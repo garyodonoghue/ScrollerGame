0b79f70572f5b3aa43ef895cdbd8b287497fd750
garyodonoghue <gary.odonoghue@gmail.com>
2013-06-15 18:41:27 +0100

    Refactor, add touch recognition instead of drag

--- test/test/test/Game1.cs
+++ test/test/test/Game1.cs
@@ -22,9 +22,13 @@ namespace test
      
         //Sprite object
         Sprite mSprite;
+        
+        float gravity;
+        bool pressed;
 
         Vector2 spritePosition1;
         Vector2 spriteSpeed1 = new Vector2(50.0f, 50.0f);
+
         Texture2D mSpriteTexture;
 
         Sprite mBackgroundOne;
@@ -32,6 +36,8 @@ namespace test
         Sprite mBackgroundThree;
         Sprite mBackgroundFour;
         Sprite mBackgroundFive;
+
+        Sprite mSkyscraper;
 
         public Game1()
         {
@@ -45,17 +51,19 @@ namespace test
             InactiveSleepTime = TimeSpan.FromSeconds(1);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
+            // Windows 8 Touch Gestures for 
+            gravity = 0F;
+            pressed = false;
+
             //Enable the FreeDrag gesture
             TouchPanel.EnabledGestures = GestureType.FreeDrag;
+            initSprites();
+        }
 
+        private void initSprites()
+        {
             // TODO: Add your initialization logic here
             mSprite = new Sprite();
 
@@ -73,23 +81,29 @@ namespace test
 
             mBackgroundFive = new Sprite();
             mBackgroundFive.Scale = 2.0f;
+
+            mSkyscraper = new Sprite();
+            mSkyscraper.Scale = 2.0f;
+
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
  
-            // TODO: use this.Content to load your game content here
-            mSpriteTexture = mSprite.LoadContent(this.Content, "ironman");
-       
             spritePosition1.X = 0;
             spritePosition1.Y = 150;
+
+            loadSprites();
+ 
+        }
+
+        private void loadSprites()
+        {
+            // TODO: use this.Content to load your game content here
+            mSpriteTexture = mSprite.LoadContent(this.Content, "ironman");
 
             mBackgroundOne.LoadContent(this.Content, "Background01");
             mBackgroundOne.Position = new Vector2(0, 0);
@@ -105,50 +119,69 @@ namespace test
 
             mBackgroundFive.LoadContent(this.Content, "Background05");
             mBackgroundFive.Position = new Vector2(mBackgroundFour.Position.X + mBackgroundFour.Size.Width, 0);
- 
+
+            mSkyscraper.LoadContent(this.Content, "skyscraper");
+            mSkyscraper.Position = new Vector2(mBackgroundFour.Position.X + mBackgroundFour.Size.Width, 100);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Windows 8 Touch Gestures for MonoGame
-            float gravity = 5F;
-            
-            while (TouchPanel.IsGestureAvailable)
+            checkBackButton();
+
+            if (TouchPanel.IsGestureAvailable)
             {
                 GestureSample gesture = TouchPanel.ReadGesture();
 
-                //The user briefly touched a single point on the screen, invert gravity so that the avatar flies upwards
-                if (gesture.GestureType == GestureType.FreeDrag)
+                Vector2 pos;
+                TouchLocationState state;
+
+                TouchCollection collection = TouchPanel.GetState();
+
+                if (collection.Count != 0)
                 {
-                    //Console.WriteLine("Tapped!");
-                    //Console.ReadLine();
-                    gravity = gesture.Delta.Y;
+                    pos = collection[0].Position;
+                    int id = collection[0].Id;
+                    state = collection[0].State;
+
+                    if (state == TouchLocationState.Pressed)
+                    {
+                        spritePosition1.Y = spritePosition1.Y - 10;
+
+                    }
+                    else if (state != TouchLocationState.Pressed)
+                    {
+                        spritePosition1.Y = spritePosition1.Y - 8;
+                    }
                 }
             }
+            else
+            {
+                spritePosition1.Y = spritePosition1.Y + 6;
+            }
+            
+            checkCollision(ref spritePosition1, ref spriteSpeed1);
 
-            // Allow the game to exit.
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
-                ButtonState.Pressed)
-                this.Exit();
+            resortBackgroundSprites();
+            adjustBackgroundSpritePositions(gameTime);           
 
-            // Move the sprite around.
-            UpdateSprite(gameTime, ref spritePosition1, ref spriteSpeed1, gravity);
-            //CheckForCollision();
+            base.Update(gameTime);
+        }
 
+        private void adjustBackgroundSpritePositions(GameTime gameTime)
+        {
+            Vector2 aDirection = new Vector2(-3, 0);
+            Vector2 aSpeed = new Vector2(160, 0);
+
+            mBackgroundOne.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            mBackgroundTwo.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            mBackgroundThree.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            mBackgroundFour.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            mBackgroundFive.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            mSkyscraper.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        private void resortBackgroundSprites()
+        {
             if (mBackgroundOne.Position.X < -mBackgroundOne.Size.Width)
             {
                 mBackgroundOne.Position.X = mBackgroundFive.Position.X + mBackgroundFive.Size.Width;
@@ -174,22 +207,23 @@ namespace test
                 mBackgroundFive.Position.X = mBackgroundFour.Position.X + mBackgroundFour.Size.Width;
             }
 
-            Vector2 aDirection = new Vector2(-3, 0);
-            Vector2 aSpeed = new Vector2(160, 0);
-
-            mBackgroundOne.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            mBackgroundTwo.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            mBackgroundThree.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            mBackgroundFour.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            mBackgroundFive.Position += aDirection * aSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;            
-
-            base.Update(gameTime);
+            if (mSkyscraper.Position.X < -mBackgroundFour.Size.Width)
+            {
+                mSkyscraper.Position.X = mBackgroundFour.Position.X + mSkyscraper.Size.Width;
+            }
         }
 
-        void UpdateSprite(GameTime gameTime, ref Vector2 spritePosition, ref Vector2 spriteSpeed, float gravity)
+        private void checkBackButton()
         {
-            spritePosition.Y += gravity;
-            
+            // Allow the game to exit.
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+                ButtonState.Pressed)
+                this.Exit();
+        }
+
+        private void checkCollision(ref Vector2 spritePosition, ref Vector2 spriteSpeed1)
+        {
+
             int MaxX =
                 graphics.GraphicsDevice.Viewport.Width - mSpriteTexture.Width;
             int MinX = 0;
@@ -197,31 +231,36 @@ namespace test
                 graphics.GraphicsDevice.Viewport.Height - mSpriteTexture.Height;
             int MinY = 0;
 
+            //Check if the sprite has hit the skyscraper, allow a margin of error of 5, and hard-coded height 
+            if (((spritePosition.X <= (mSkyscraper.Position.X) + 5) && (spritePosition.X >= (mSkyscraper.Position.X) - 5)) && spritePosition.Y >= 50)
+            {
+                System.Diagnostics.Debug.WriteLine("Hit the skyscraper");
+            }
+
             // Check for bounce.
             if (spritePosition.X > MaxX)
             {
-                spriteSpeed.X *= -1;
+                spriteSpeed1.X *= -1;
                 spritePosition.X = MaxX;
             }
 
             else if (spritePosition.X < MinX)
             {
-                spriteSpeed.X *= -1;
+                spriteSpeed1.X *= -1;
                 spritePosition.X = MinX;
             }
 
             if (spritePosition.Y > MaxY)
             {
-                spriteSpeed.Y *= -1;
+                spriteSpeed1.Y *= -1;
                 spritePosition.Y = MaxY;
             }
 
             else if (spritePosition.Y < MinY)
             {
-                spriteSpeed.Y *= -1;
+                spriteSpeed1.Y *= -1;
                 spritePosition.Y = MinY;
             }
-
         }
 
         /// <summary>
@@ -240,6 +279,7 @@ namespace test
             mBackgroundThree.Draw(this.spriteBatch);
             mBackgroundFour.Draw(this.spriteBatch);
             mBackgroundFive.Draw(this.spriteBatch);
+            mSkyscraper.Draw(this.spriteBatch);
 
             spriteBatch.Draw(mSpriteTexture, spritePosition1, Color.White);
             
